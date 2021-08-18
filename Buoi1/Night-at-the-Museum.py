fd604c3e97e645c4ac123cc3c50629b588415fa8
huytunguyenn <49774218+huytunguyenn@users.noreply.github.com>
2021-08-18 23:02:10 +0700

    add

--- Buoi1/Night-at-the-Museum.py
+++ Buoi1/Night-at-the-Museum.py
@@ -2,8 +2,13 @@
 Cho 1 vòng tròn chữ cái, kiểu như quay số điện thoại ngày xưa (ngoại trừ nó ko tự quay lại vị trí ban đầu)
 Input là 1 string
 Output là số nhỏ nhất tổng các vòng xoay để in ra string đó
+Có 26 chữ cái, bắt đầu tại 'a'.
 
-
+IDEA:
+ - init tại pos = 'a'
+ - tính l1 và l2 là khoảng cách giữa 'a' và chữ cái (từ 2 phía)
+ - chọn min của l1,l2 rồi + vô count
+ - cập nhật pos mới là vị trí chữ cái hiện tại
 """
 
 if __name__ == '__main__':
@@ -11,7 +16,13 @@ if __name__ == '__main__':
     chars = [char for char in name]
 
     count = 0
-    pos = 0
+    pos = 97  # init at 'a'
 
     for char in chars:
+        l1 = abs(ord(char) - pos)
+        l2 = 26 - l1
+        l = min(l1, l2)
+        count += l
+        pos = ord(char)
 
+    print(count)
