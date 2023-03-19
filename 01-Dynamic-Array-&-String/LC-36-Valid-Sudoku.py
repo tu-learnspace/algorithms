78be7a4bcdbf4283ad7d53d2522bae705c34b3b8
HuyTu <dragon10x0408@gmail.com>
2023-03-19 23:24:37 +0700

    set

--- 01-Dynamic-Array-&-String/LC-36-Valid-Sudoku.py
+++ 01-Dynamic-Array-&-String/LC-36-Valid-Sudoku.py
@@ -1,7 +1,14 @@
 """
 https://leetcode.com/problems/valid-sudoku
 
+Cho sudoku 9x9, kiểm tra nếu sudoku valid:
+- hàng phải chứa 1->9
+- cột phải chứa 1->9
+- trong ô 3x3 phải chứa 1->9
 
+IDEA:
+các criteria đều check phần tử ko lặp lại
+-> sài set, tạo arr 3 phần tử là 3 set tương ứng vs các criteria
 """
 
 
@@ -10,6 +17,14 @@ def isValidSudoku(board):
     :type board: List[List[str]]
     :rtype: bool
     """
+    check = []
+    for i in range(9):
+        for j in range(9):
+            element = board[i][j]
+            if element != ".":  # mỗi tuple phải khác nhau format để không bị lẫn
+                check += [(i, element), (element, j), (i // 3, j // 3, element)] # dùng check.extend cũng được
+
+    return len(check) == len(set(check))
 
 
 if __name__ == '__main__':
